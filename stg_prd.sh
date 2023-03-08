607f8ad39eb84a0d5bef46146d00f8842e0a4d28
Nivetha <you@example.com>
2023-03-08 15:19:35 +0300

    3

--- stg_prd.sh
+++ stg_prd.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-<<<<<<< HEAD
 sudo sheckout prd
 sudo git merge stg  
-=======
 sudo git checkout prd
 sudo git merge stg
->>>>>>> dev
 sudo git tag `date "+%Y%m%d%H%M%S"` 
 sudo git push --tags
 sudo git push 
 sudo git checkout stg
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
